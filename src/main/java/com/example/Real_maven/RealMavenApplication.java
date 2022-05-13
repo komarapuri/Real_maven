cd0fcf3bc157ed2ae3d80b36225dd840537b95f4
komarapuri <phanindhrak2000@gmail.com>
2022-05-13 18:51:11 +0530

    this is my first commit

---
+++ src/main/java/com/example/Real_maven/RealMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Real_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealMavenApplication.class, args);
+	}
+
+}
